d02a5fdda2e3a716e70d605f2261dd8980ca9f73
Samay10 <75678485+Samay10@users.noreply.github.com>
2022-09-22 23:07:16 +0530

    Update kadane_algo.cpp

--- kadane_algo.cpp
+++ kadane_algo.cpp
@@ -1,4 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 int maxSubArray(vector<int>& nums, vector<int>& subarray){
+    int msf = INT_MIN, meh = 0;
+    int s = 0;
+    for(int i=0;i<nums.size();i++){
+        meh += nums[i];
+        if(meh > msf){
+            subarray.clear();
+            msf = meh;
+            subarray.push_back(s);
+            subarray.push_back(i);
+        }
+        if(meh < 0){
+            meh = 0;
+            s = i+1;
+        }
+    }
+    return msf;
+}
+
+int main(){
     int msf = INT_MIN, meh = 0;
     int s = 0;
     for(int i=0;i<nums.size();i++){
